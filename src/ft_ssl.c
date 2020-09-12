8e14c1fc0aa930d35547b62d65a25d2db2880a88
Drew Finnis <drewfinnis@gmail.com>
2020-09-12 15:22:34 +0200

    feature/adapted gnl to read whole file

--- src/ft_ssl.c
+++ src/ft_ssl.c
@@ -1,5 +1,61 @@
 #include "../inc/ft_ssl.h"
 
+static int	ft_get_line(char **line, char *str)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	tmp = *line;
+	if (!(*line = ft_strjoin(*line, str)))
+		return (-1);
+	ft_freestr(tmp);
+	ft_freestr(str);
+	return (0);
+}
+
+static int	ft_gnl_error(char **line, char *str)
+{
+	int	g;
+
+	g = 0;
+	if ((g = ft_get_line(line, str)) == 1)
+		return (1);
+	else if (g == -1)
+	{
+		ft_freestr(str);
+		return (-1);
+	}
+	return (0);
+}
+
+int			get_next_line2(const int fd, char **line)
+{
+	char	*str;
+	int		k;
+	int		g;
+
+	if (line == NULL || fd < 0 || !(*line = ft_strdup("")))
+		return (-1);
+	k = 1;
+	while (k > 0)
+	{
+		if (!(str = ft_strnew(1)))
+			return (-1);
+		if ((k = read(fd, str, 1)) == -1)
+			exit(1);
+		if ((g = ft_gnl_error(line, str)) == 1)
+			return (1);
+		else if (g == -1)
+			return (-1);
+		if (k == 0 && !*line[0])
+			return (0);
+		else if (k == 0 && *line[0])
+			return (1);
+	}
+	return (-1);
+}
+
+
 void	print_usage(void)
 {
 	ft_putstr("\nusage:\t./ft_ssl []");
@@ -22,6 +78,7 @@ int		read_arg(char **argv, t_args *args, int i)
 	{
 		ft_printf("arg: %s\n", argv[i]);//
 		char *line;
+		int	error;
 		int	fd;
 
 		line = NULL;
@@ -30,6 +87,8 @@ int		read_arg(char **argv, t_args *args, int i)
 			ft_printf("ft_ssl: md5/sha: %s: %s\n", argv[i], strerror(errno));/// command md5/sha etc!!!
 			//error exit
 		}
+		error = get_next_line2(fd, &line);
+		ft_printf("error: %d\n", error);//
 		ft_printf("fd: %d\n", fd);//
 		ft_printf("line: %s\n", line);//
 		// parse file
