ae954ea6c2cd1d4f8299afa74ce273094e81c242
Drew Finnis <drewfinnis@gmail.com>
2020-10-13 18:17:09 +0200

    refacto/calloc clarity

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -5,9 +5,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void *ptr;
 
-	ptr = malloc(count * size);
-	if (ptr == NULL)
-		return (ptr);
+	if (!(ptr = malloc(count * size)))
+		return (NULL);
 	ft_bzero(ptr, size * count);
 	return (ptr);
 }
